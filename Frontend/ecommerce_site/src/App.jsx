4d99c3799604de972841823e7547ab934bfd5d0d
Deepakshi725 <126474821+Deepakshi725@users.noreply.github.com>
2025-03-18 13:13:26 +0530

    completed milestone_25

--- Frontend/ecommerce_site/src/App.jsx
+++ Frontend/ecommerce_site/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 // eslint-disable-next-line 
 import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import {LoginPage, SignupPage, Home, CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress, SelectAddress} from "./Routes/routes.js";
+import {LoginPage, SignupPage, Home, CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress, SelectAddress, OrderConfirmation} from "./Routes/routes.js";
 
 const App = () => {
   return (
@@ -20,6 +20,7 @@ const App = () => {
       <Route path="/profile" element={<Profile />} />
       <Route path="/create-address" element={<CreateAddress />} />
       <Route path="/select-address" element={<SelectAddress />} />
+      <Route path="/order-confirmation" element={<OrderConfirmation />} />
 
     </Routes>
     </BrowserRouter>
