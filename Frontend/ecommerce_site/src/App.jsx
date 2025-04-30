070332e6bb1a590ea22912ea76a2b38786628463
Deepakshi725 <126474821+Deepakshi725@users.noreply.github.com>
2025-04-30 11:55:08 +0530

    Done and Dusted finally

--- Frontend/ecommerce_site/src/App.jsx
+++ Frontend/ecommerce_site/src/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/' element={<LoginPage/>}/>
+      <Route path='/login' element={<Home/>}/>
       <Route path='/signup' element={<SignupPage/>}/>
       <Route path='/create-product' element={<CreateProduct/>}/>
       <Route path="/myproducts" element={<MyProducts/>} />
